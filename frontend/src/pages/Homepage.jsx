f9346b8e6951987afeaeed89e05bf086d22c49cd
Amrit_Xtha <keshwor12@gmail.com>
2025-08-15 15:50:27 +0545

    add a component in homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import Particles  from '../Reactbits/Particle'
+import  Particles  from '../Reactbits/Particle'
+
 
 const Homepage = () => {
+
+  
+
   return (
     <div style={{ width: '100%', height: '100dvh', position: 'relative' }}>
     <Particles
@@ -13,7 +17,14 @@ const Homepage = () => {
       moveParticlesOnHover={true}
       alphaParticles={true}
       disableRotation={false}
+
     />
+
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white flex flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Welcome to Believe Me</h1>
+      <p className="text-lg mt-4">The AI Interviewer</p>
+     <button className="bg-transparent h-12 w-40 text-white cursor-target px-6 py-3 font-bold rounded-full border-2 hover:bg-gray-100 transition-colors hover:text-black text-xl" >Get Started</button>  
+    </div>
   </div>
   )
 }
